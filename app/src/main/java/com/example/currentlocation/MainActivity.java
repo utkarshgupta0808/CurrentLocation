1e8005e555d69cfac72d66b42e56779a03b1f91d
Utkarsh Gupta <utkarshgupta0808@gmail.com>
2020-05-25 15:33:08 +0530

    pehla commit

---
+++ app/src/main/java/com/example/currentlocation/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.currentlocation;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    Geocoder geocoder;
+    List<Address> addresses;
+    FusedLocationProviderClient fusedLocationProviderClient;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView=  (TextView) findViewById(R.id.textView);
+
+        fusedLocationProviderClient= LocationServices.getFusedLocationProviderClient(this);
+
+        fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task) {
+                Location location=task.getResult();
+                if(location!=null){
+                    try {
+                        geocoder= new Geocoder(MainActivity.this,Locale.getDefault());
+                        addresses=geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
+                        String address= addresses.get(0).getAddressLine(0);
+                        String area = addresses.get(0).getLocality();
+                        String city= addresses.get(0).getAdminArea();
+                        String  country= addresses.get(0).getCountryName();
+                        String  postalcode= addresses.get(0).getPostalCode();
+
+                        String fulladdress= address+", "+area+", "+city+", "+country+", "+postalcode;
+                        textView.setText(fulladdress);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        });
+
+
+
+
+
+    }
+}
